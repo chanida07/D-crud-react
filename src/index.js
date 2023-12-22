3a279e8f98b016325ad21335a202caffda2c57ed
chanida07 <151849586+chanida07@users.noreply.github.com>
2023-12-22 18:10:34 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter } from 'react-router-dom';
+//import { NativeRouter } from "react-router-native";
+//เพิ่ม BrowserRouter 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+  root.render(
+    //เปลี่ยนเป็นBrowserRouter 
+    <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter >
+  //document.getElementById(root)
 );
 
 // If you want to start measuring performance in your app, pass a function
